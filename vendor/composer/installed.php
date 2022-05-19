2c4789fe5e00c22c5432a05c8a22cccc5208c080
Kazimieras Babrauskas <kazimieras.babrauskas@printplius.lt>
2022-05-19 15:29:51 +0300

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '82872925bfabe91f102a0e576dfefa4229eae8d3',
+        'name' => 'printplius/module_skeleton',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'printplius/module_skeleton' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '82872925bfabe91f102a0e576dfefa4229eae8d3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
